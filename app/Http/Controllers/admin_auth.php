345ec0cd14b135408998bcf7db313f65e5a3a9bf
Zillemudasar2158 <zillemudasar60@gmail.com>
2025-05-22 04:49:22 +0500

    admin page login error setting

--- app/Http/Controllers/admin_auth.php
+++ app/Http/Controllers/admin_auth.php
@@ -119,7 +119,7 @@ class admin_auth extends Controller
         ]);
         $email=$request->input('email');
         $password=$request->input('password');
-        if ($email=='zillemudasar2158@gmail.com' && $password=='12345') {
+        if ($email=='zillemudasar60@gmail.com' && $password=='12345') {
             $request->session()->put('name','zillemudasar');
             return redirect('usersdata');
         }
